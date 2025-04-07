60ea387cc60193c51eedbc079713aea759e69ba1
Dominik M <phoenix.dh-m16@outlook.com>
2025-04-07 20:15:58 +0200

    add start screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+
+export const routes: Routes = [
+    { path: "", component: StartScreenComponent },
+    { path: "game", component: GameComponent }
+];
